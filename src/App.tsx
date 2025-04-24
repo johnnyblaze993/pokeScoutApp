b489c16afe60c927afc3131c71c974cb51d339ad
Jonathan <alvarezjo@astrion.us>
2025-04-24 13:08:47 -0500

    renamed files to include 'Page' in name

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import PokemonList from "./features/pokemon/pages/PokemonList";
-import PokemonDetail from "./features/pokemon/pages/PokemonDetail";
-import TeamBuilder from "./features/teamBuilder/pages/TeamBuilder";
+import PokemonList from "./features/pokemon/pages/PokemonListPage";
+import PokemonDetail from "./features/pokemon/pages/PokemonDetailPage";
+import TeamBuilder from "./features/teamBuilder/pages/TeamBuilderPage";
 import Favorites from "./features/favorites/pages/FavoritesPage";
 import NotFound from "./pages/NotFound";
 import Header from "./components/Header/Header";
 import AppLayout from "./layouts/AppLayout";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/pokemon" element={<PokemonList />} />
-          <Route path="/pokemon/:id" element={<PokemonDetail />} />
-          <Route path="/team-builder" element={<TeamBuilder />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route element={<AppLayout />}>
+					<Route path="/" element={<Home />} />
+					<Route path="/pokemon" element={<PokemonList />} />
+					<Route path="/pokemon/:id" element={<PokemonDetail />} />
+					<Route path="/team-builder" element={<TeamBuilder />} />
+					<Route path="/favorites" element={<Favorites />} />
+					<Route path="*" element={<NotFound />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
